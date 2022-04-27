88bb970c80387b88d7bba766a3f8ce347f170b13
shan5742 <asamshan456@gmail.com>
2022-04-27 21:35:49 +0100

    removing comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,8 +21,6 @@ function App() {
   const web3ModalRef = useRef();
 
   const getProviderOrSigner = async (needSigner = false) => {
-    // Connect to Metamask
-    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
     const provider = await web3ModalRef.current.connect();
     const web3Provider = new ethers.providers.Web3Provider(provider);
     const signer = web3Provider.getSigner();
@@ -32,7 +30,6 @@ function App() {
     const balanceInEth = ethers.utils.formatEther(balance);
     setUserBalance(balanceInEth);
 
-    // If user is not connected to the Rinkeby network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
     if (chainId !== 4) {
       setNetworkError(true);
@@ -122,10 +119,7 @@ function App() {
   };
 
   useEffect(() => {
-    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
     if (!walletConnected) {
-      // Assign the Web3Modal class to the reference object by setting it's `current` value
-      // The `current` value is persisted throughout as long as this page is open
       web3ModalRef.current = new Web3Modal({
         network: "rinkeby",
         providerOptions: {},
